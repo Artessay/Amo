3e284cec351685fc802ca96ee0f976ed454af0ac
Artessay <rihong.edu@outlook.com>
2025-12-11 18:05:17 +0800

    recover sync mode for vllm rollout

--- verl/trainer/main_ppo.py
+++ verl/trainer/main_ppo.py
@@ -140,11 +140,12 @@ class TaskRunner:
             self.mapping[role] = "global_pool"
             return actor_rollout_cls, ray_worker_group_cls
 
-        if config.actor_rollout_ref.rollout.mode == "sync":
-            raise ValueError(
-                "Rollout mode 'sync' has been removed. Please set "
-                "`actor_rollout_ref.rollout.mode=async` to use the native server rollout."
-            )
+        # if config.actor_rollout_ref.rollout.mode == "sync":
+        #     raise ValueError(
+        #         "Rollout mode 'sync' has been removed. Please set "
+        #         "`actor_rollout_ref.rollout.mode=async` to use the native server rollout."
+        #     )
+        print(f"[Amo] actor rollout ref mode: {config.actor_rollout_ref.rollout.mode}")
 
         if config.actor_rollout_ref.actor.strategy in {"fsdp", "fsdp2"}:
             from verl.workers.fsdp_workers import ActorRolloutRefWorker, AsyncActorRolloutRefWorker
