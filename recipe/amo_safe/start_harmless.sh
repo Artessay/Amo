0249c1684da4237476cf832d86cde86bf37a7a5e
Artessay <rihong.edu@outlook.com>
2025-12-22 13:48:13 +0000

    remove calibration for safe

--- recipe/amo_safe/start_harmless.sh
+++ recipe/amo_safe/start_harmless.sh
@@ -6,9 +6,8 @@ WORKSPACE=$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")
 
 REWARD_MODEL_PATH=$WORKSPACE/playground/reward_model/checkpoints/Qwen2.5-7B-SafeRLHF-CM
 PORT=50052
-CALIBRATION_PATH=$WORKSPACE/playground/reward_calibration/config/Qwen2.5-7B-SafeRLHF-CM_test_calibration.json
+# CALIBRATION_PATH=$WORKSPACE/playground/reward_calibration/config/Qwen2.5-7B-SafeRLHF-CM_test_calibration.json
 
 python3 $WORKSPACE/recipe/amo_safe/reward_server.py \
     --model_path $REWARD_MODEL_PATH \
-    --port $PORT \
-    --calibration_path $CALIBRATION_PATH
+    --port $PORT 
