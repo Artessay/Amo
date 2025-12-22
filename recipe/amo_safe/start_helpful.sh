d6db51c24971a4ed1e706d8f4a4d8020cdf97de2
Artessay <rihong.edu@outlook.com>
2025-12-22 07:51:06 +0000

    result does not meet expection, retry

--- recipe/amo_safe/start_helpful.sh
+++ recipe/amo_safe/start_helpful.sh
@@ -6,5 +6,9 @@ WORKSPACE=$(dirname "$(dirname "$(dirname "$(realpath "${BASH_SOURCE[0]}")")")")
 
 REWARD_MODEL_PATH=$WORKSPACE/playground/reward_model/checkpoints/Qwen2.5-7B-SafeRLHF-RM
 PORT=50051
+CALIBRATION_PATH=$WORKSPACE/playground/reward_calibration/config/Qwen2.5-7B-SafeRLHF-RM_test_calibration.json
 
-python $WORKSPACE/recipe/amo_safe/reward_server.py --model_path $REWARD_MODEL_PATH --port $PORT
+python3 $WORKSPACE/recipe/amo_safe/reward_server.py \
+    --model_path $REWARD_MODEL_PATH \
+    --port $PORT \
+    --calibration_path $CALIBRATION_PATH
