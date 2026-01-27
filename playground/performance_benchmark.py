755304771aacd78d745d3dd102acfa7db9b2bbc0
Artessay <rihong.edu@outlook.com>
2026-01-27 16:45:03 +0000

    something

--- playground/performance_benchmark.py
+++ playground/performance_benchmark.py
@@ -1,29 +1,24 @@
 import torch
 import time
+import random
 
 
 def test_gpu_performance(device_id):
     device = torch.device(f'cuda:{device_id}')
 
     # Create two large matrices
-    size = 50000 # 36000
+    size = random.randint(48000, 64000) # 60000 # 36000
     a = torch.randn(size, size, device=device)
     b = torch.randn(size, size, device=device)
 
-    # Warm-up
-    for _ in range(100):
-        c = torch.matmul(a, b)
-
-    return
-
     # Start timing
     start_time = time.time()
-    for _ in range(1000):
+    for _ in range(100):
         c = torch.matmul(a, b)
     end_time = time.time()
 
     elapsed_time = end_time - start_time
-    print(f"GPU {device_id} took {elapsed_time:.4f} seconds to run 1000 matrix multiplications.")
+    print(f"GPU {device_id} took {elapsed_time:.4f} seconds to run 100 matrix multiplications.")
     return elapsed_time
 
 
@@ -36,6 +31,8 @@ if __name__ == "__main__":
         while True:
             for i in range(num_gpus):
                 test_gpu_performance(i)
+                sleep_time = random.uniform(0.05, 0.5)
+                time.sleep(sleep_time)
         # total_time = 0.0
         # for i in range(num_gpus):
         #     total_time += test_gpu_performance(i)
