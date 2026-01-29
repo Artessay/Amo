7f5277dc0a4046663386f215ffe3eaabbc48e450
Artessay <rihong.edu@outlook.com>
2026-01-29 16:17:14 +0000

    update qwen2.5 3b result, showing 1e-5 is better

--- scripts/eval_math.sh
+++ scripts/eval_math.sh
@@ -17,11 +17,11 @@ EXPERIMENTS=(
     # "qwen2.5-3b"
     # "qwen2.5-3b_grpo"
     # "qwen2.5-3b_gdpo"
-    # "qwen2.5-3b_hvpo"
+    "qwen2.5-3b_hvpo"
 
     # "llama3.2-3b"
-     "llama3.2-3b_grpo"
-     "llama3.2-3b_gdpo"
+    # "llama3.2-3b_grpo"
+    # "llama3.2-3b_gdpo"
     # "llama3.2-3b_hvpo"
 
     # "qwen2.5-1.5b_hvpo_distance"
